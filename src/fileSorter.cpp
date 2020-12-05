4db5a874803c899663b96edbd238e51b5a9d1954
Kirill Smirnov <smk-robotics@gmail.com>
2020-12-05 23:00:51 +0300

    Add chunk deleting function. Update merge sorting function. Code refactoring.

--- src/fileSorter.cpp
+++ src/fileSorter.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <fstream>
+#include <iomanip>
 #include <memory>
 #include <set>
-#include <map>
-#include <vector>
+#include <unordered_map>
 #include <math.h>
 #include <string>
 #include <cstring>
@@ -21,68 +21,98 @@ inline void showChunk(const T &chunk) noexcept {
 }
 
 template <typename T>
-inline void writeChunkToFile(const T &chunk, const std::string chunkFileName) noexcept {
-  std::cout << "[FileSorter] - Create chunk # " << chunkFileName << ". Size - " <<
-                            chunk.size() * charBufferSize / pow(1024,2) << " Mb" << std::endl;
+inline void writeDataToFile(const T &data, const std::string chunkFileName) noexcept {
   std::ofstream chunkFile(chunkFileName);
-  for (const auto &element:chunk) {
+  for (const auto &element:data) {
     chunkFile << element;
   }
   chunkFile.close();
 }
 
+template <typename T>
+inline void deleteFiles(T &filesMap) {
+  for (auto i = 0; i <= filesMap.size() + 1; ++i) {
+    if (remove(filesMap.begin()->first.c_str()) == 0) {
+      filesMap.erase(filesMap.begin());
+    } else {
+      std::cerr << std::setw(80) << std::left <<
+      "[FileSorter] - Can't remove \"" + filesMap.begin()->first + "\" chunk:" << std::setw(10) << std::right <<
+                                                                                                "[ERROR]" << std::endl;
+    }
+  }
+}
+
 // Split data from given file to sorted chunks (files).
-std::vector<std::string> splitFileToChunks(const std::string &fileName) noexcept {
+std::unordered_map<std::string, unsigned long> splitFileToChunks(const std::string &fileName) noexcept {
   std::string currentChunkName;
-  std::vector<std::string> chunksNameList;
+  std::unordered_map<std::string, unsigned long> chunksList;
   char charBuffer[charBufferSize + 1];    // Buffer for readed data.
   std::multiset<std::string> sortedChunk; // Sorted chunk of data from given file.
   std::ifstream originFile(fileName);     // Open given file to read data.
-  std::cout << "[FileSorter] - Splitting data from " << fileName << " to chunks - [START]" << std::endl;
   auto currentChunkIndex = 0;             // Index of current chunk.
   if (originFile) {                       // Check that given file is exist and successfully opened.
     while (!originFile.eof()) {
-      originFile.read(charBuffer, charBufferSize);                 // Read data from file with unsorted data.
-      sortedChunk.insert(charBuffer);                              // Add data to sorted container.
-      if (sortedChunk.size() >= chunkSizeLimit) {                  // Check sorted container size.
+      originFile.read(charBuffer, charBufferSize);                // Read data from file with unsorted data.
+      sortedChunk.insert(charBuffer);                             // Add data to sorted container.
+      if (sortedChunk.size() >= chunkSizeLimit) {                 // Check sorted container size.
         currentChunkName = std::to_string(currentChunkIndex);
-        chunksNameList.push_back(currentChunkName);                // Store name of current chunk.
-        writeChunkToFile(sortedChunk, currentChunkName);           // Output sorted chunk to external file.
+        chunksList.insert({currentChunkName, 0});                   // Store name of chunk and number of readed bytes.
+        writeDataToFile(sortedChunk, currentChunkName);           // Output sorted chunk to external file.
         sortedChunk.erase(sortedChunk.begin(), sortedChunk.end()); // Clear sorted chunk to repeat process.
         ++currentChunkIndex;
       }
     }
   } else {
-    std::cerr << "[FileSorter] - Can't open file " << fileName << std::endl;
+    std::cerr << std::setw(80) << std::left <<
+    "[FileSorter] - Can't open file \"" + fileName + "\":" << std::setw(10) << std::right << "[ERROR]" << std::endl;
+    return {};
   }
   currentChunkName = std::to_string(currentChunkIndex);
-  chunksNameList.push_back(currentChunkName);
-  writeChunkToFile(sortedChunk, currentChunkName);
+  chunksList.insert({currentChunkName, 0});
+  writeDataToFile(sortedChunk, currentChunkName);
   originFile.close();
-  std::cout << "[FileSorter] - Splitting data to chunks - [FINISHED]" << std::endl;
-  return chunksNameList;
+  return chunksList;
 }
 
-// void mergeSortFromChunks(const std::vector<std::string> *chunksList, const std::string &sortedFileName) noexcept {
-void mergeSortFromChunks(const std::vector<std::string> *chunksList, const std::string &sortedFileName) noexcept {
-  std::cout << "[FileSorter] - Merge sort from " << chunksList->size() << " chunks - [START]" << std::endl;
-  std::ifstream resultFile(sortedFileName); // Result file with sorted data.
+void mergeSortFromChunks(const std::unordered_map<std::string, unsigned long> &chunksList,
+                         const std::string &sortedFileName) noexcept {
+  std::ifstream resultFile(sortedFileName); // Open file to load result sorted data.
   char charBuffer[charBufferSize + 1];      // Buffer for readed data.
   std::multiset<std::string> sortedData;    // Sorted part of data from all chunks.
+  unsigned long iterationIndex = 0;
   for (const auto &chunk:chunksList) {
-    for (auto i = 0; i < (chunkSizeLimit / chunksList.size(); ++i) {
-      std::ifstream currentChunkFile(chunk);
-      currentChunkFile.read(charBuffer, sortedData);
+    std::ifstream currentChunkFile(chunk.first);
+    currentChunkFile.seekg(chunk.second);
+    while (!currentChunkFile.eof()) {
+      currentChunkFile.read(charBuffer, charBufferSize);
       sortedData.insert(charBuffer);
     }
+    writeDataToFile(sortedData, sortedFileName);
+    currentChunkFile.close();
+    ++iterationIndex;
   }
-  std::cout << "[FileSorter] - Merge sort from chunks - [FINISHED]" << std::endl;
 }
 
 int main() {
-  std::unique_ptr<std::vector<std::string>> chunks;
-  chunks = std::make_unique<std::vector<std::string>>(splitFileToChunks("test-data/TestDataFile"));
+  std::string originFile = "test-data/TestDataFile";
+  std::unordered_map<std::string, unsigned long> chunks;
+  chunks = splitFileToChunks(originFile);
+
+  if (chunks.empty()) {
+      return 0;
+  }
+  std::cout << std::setw(80) << std::left <<
+              "[FileSorter] - Splitting \"" + originFile + "\" file to chunks:" << std::setw(10) <<
+                                                                              std::right << "[FINISHED]" << std::endl;
   std::string sortedFileName = "sortedData";
-  mergeSortFromChunks(chunks.get(), sortedFileName);
+  mergeSortFromChunks(chunks, sortedFileName);
+  std::cout << std::setw(80) << std::left <<
+               "[FileSorter] - Merge sorting from " + std::to_string(chunks.size()) + " chunks:" << std::setw(10) <<
+                                                                              std::right << "[FINISHED]" << std::endl;
+  deleteFiles(chunks);
+  if (chunks.empty()) {
+  std::cout << std::setw(80) << std::left <<
+               "[FileSorter] - Deleting chunk files:" << std::setw(10) << std::right << "[FINISHED]" << std::endl;
+  }
   return 0;
 }
